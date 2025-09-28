6ac5d8b417ab97008759627b9ec32e68fdaed8aa
Ketan-ship-it <wolfpackinfin@gmail.com>
2025-09-29 00:52:56 +0530

    initial commit

---
+++ Payment-Method/src/main/java/com/Fintech/Payment_Method/PaymentMethodApplication.java
@@ -0,0 +1,13 @@
+package com.Fintech.Payment_Method;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMethodApplication.class, args);
+	}
+
+}
